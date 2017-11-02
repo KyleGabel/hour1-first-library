96370eea2f2adc18959c31f3190bbbd3a60fa131
KyleGabel <31696075+KyleGabel@users.noreply.github.com>
2017-11-02 08:11:21 -0500

    Update converterlib.h

--- converterlib.h
+++ converterlib.h
@@ -5,3 +5,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float inches_to_meaters(void)
+{
+    float inches = get_float("number of inches: \n");
+    float meather = inches * 0.0254;
+    printf("\n The number of meaters is %.2f\n", inches);
+    return inches;
+}
